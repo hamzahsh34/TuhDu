9c4d4ca063920b6fc5651f3fec35c160de11e906
hamzahs34 <hamzah_shahid@hotmail.com>
2019-08-21 19:05:07 -0400

    Persists data using UserDefaults method.

--- TuhDu/ToDoListViewController.swift
+++ TuhDu/ToDoListViewController.swift
@@ -16,7 +16,7 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Persist data with User Defaults
+        // Persist data with UserDefaults methods
         
         if let items = defaults.array(forKey: "TodoListArray") as? [String] {
             itemArray = items
